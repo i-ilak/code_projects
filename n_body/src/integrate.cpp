44b51fe2b5ea3f1f229d626688e31b68f11b0d41
iilak <iilak@student.ethz.ch>
2021-03-16 17:37:11 +0100

    added some comments for integrate routines

--- n_body/src/integrate.cpp
+++ n_body/src/integrate.cpp
@@ -1,24 +1,20 @@
 #include "integrate.hpp"
 
-Eigen::MatrixXd integrate(step_t step, rhs_t rhs, phase_t const & z0,
-                          double const & time_interval, unsigned const & bins){
-    Eigen::MatrixXd z(bins+1, z0.size());
-    z.fill(0);
-    double const t0 = 0;
-    double const dt = time_interval/bins;
-    z.row(0) = z0;
-    for(std::size_t i = 0; i<bins; ++i) {
-        z.row(i + 1) = step(rhs, z.row(i), t0 + i * dt, dt);
-    }
-    phase_t t(bins+1);
-    for(std::size_t k = 0; k<t.size(); ++k){
-        t(k) = (k+1) * dt;
-    }
-    z.conservativeResize(Eigen::NoChange, z.cols()+1);
-    z.col(z.cols()-1) = t;
-    return z;
-}
+/*
+ For both methods we divide the computation into steps. Each step gets perform N times. 
+ The following function implement this step. Later, these will be called and summed up.
+ */
 
+/*
+  PRE:
+    - rhs_t rhs:        f(t,z) for a given ODE.
+    - phase_t const z0: initial condition in phase space coordinates. 
+    - double t0:        time at which we start the step
+    - double dt:        time step, usually defined as time interval of the simulation
+                        divided by number of total steps taken in the simulation.
+  POST:
+    - phase_t z+dz:     phase space type coordinates
+ */
 phase_t explicit_euler_step(rhs_t rhs, phase_t const & z0, double t0, double dt){
     return z0 + dt * rhs(t0,z0);
 }
@@ -29,9 +25,37 @@ phase_t implicit_euler_step(rhs_t rhs, phase_t const & z0, double t0, double dt)
 }
 
 
-
+/*
+ Explicit Euler method.     For more details see: Wikipedia https://en.wikipedia.org/wiki/Euler_method
+ Explicit Midpoint method.  For more detials see: Wikipedia https://en.wikipedia.org/wiki/Midpoint_method
+ PRE:
+    rhs_t f:            characterizes the ODE. 
+    phase_t const z0:   initial conditions of the problem in form of phase-space
+                        coordinates.
+    double const T:     time interval you want to simulate, i.e. has to be a
+                        positive real number.
+    unsigned const N:   number of individual steps you want to take in simulation.
+                        The bigger N is the more precise the result will be and  
+                        the longer the computation will take. 
+ POST:
+    Eigen::MmatrixXd result:    row k contains the phase space coordinates at time k*T/N.
+ */
 Eigen::MatrixXd explicit_euler(rhs_t rhs, phase_t const &z0, double const & T, unsigned const & N){
-    return integrate(explicit_euler_step,rhs,z0,T,N);
+    Eigen::MatrixXd z(N+1, z0.size());
+    z.fill(0);
+    double const t0 = 0;
+    double const dt = T/N;
+    z.row(0) = z0;
+    for(std::size_t i = 0; i<N; ++i) {
+        z.row(i + 1) = explicit_euler_step(rhs, z.row(i), t0 + i * dt, dt);
+    }
+    phase_t t(N+1);
+    for(std::size_t k = 0; k<t.size(); ++k){
+        t(k) = (k+1) * dt;
+    }
+    z.conservativeResize(Eigen::NoChange, z.cols()+1);
+    z.col(z.cols()-1) = t;
+    return z;
 }
 Eigen::MatrixXd explicit_midpoint(rhs_t rhs, phase_t const &z0, double const & T, unsigned const & N){
     Eigen::MatrixXd z(N+1, z0.size());
--- n_body/src/integrate.hpp
+++ n_body/src/integrate.hpp
@@ -8,17 +8,36 @@
 #include <cmath>
 #include <fstream>
 
+/*
+ Define some useful types. All the names are related to physicsl quantities and
+ should be interpreted in this context. 
+ */
 using mass_t = double const;
 using name_t = std::string const;
 using vector_t = Eigen::Vector3d;
-using phase_t = Eigen::VectorXd;
+using phase_t = Eigen::VectorXd;    // type used for elements of "phase space"
+
+// The following is in relation to how ODE's are defined. If we are given the
+// most general form of an ODE, z' = f(t,z), where t is a constant (usually
+// called time) and z an element of the phase space, then rhs_t is the type of
+// f. 
 using rhs_t = phase_t (*)(double const &, phase_t const &);
-using step_t = phase_t (*)(rhs_t , phase_t const &, double, double);
+using step_t = phase_t (*)(rhs_t , phase_t const &, double, double); // redundant an can probably be eliminated 
 
-phase_t explicit_euler_step(rhs_t, phase_t const &, double, double);
-Eigen::MatrixXd integrate(step_t, rhs_t, phase_t const &, double const &, unsigned const &);
+/*
+ The following are two, well-known solution schemes for ODE's. For both the
+ following is true.
+ PRE:
+    rhs_t f:            characterizes the ODE. 
+    phase_t const z0:   initial conditions of the problem in form of phase-space
+                        coordinates.
+    double const T:     time interval you want to simulate, i.e. has to be a
+                        positive real number.
+    unsigned const N:   number of individual steps you want to take in simulation.
+                        The bigger N is the more precise the result will be and  
+                        the longer the computation will take. 
+ */
 Eigen::MatrixXd explicit_euler(rhs_t, phase_t const &, double const &, unsigned const &);
-
 Eigen::MatrixXd explicit_midpoint(rhs_t, phase_t const &, double const &, unsigned const &);
 
 #endif // INTEGRATE_HPP
