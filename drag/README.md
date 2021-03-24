c5765a8f3505904c59169814c64ade505b345324
iilak <iilak@student.ethz.ch>
2021-03-24 21:57:10 +0100

    got first results and numerical instability

--- drag/README.md
+++ drag/README.md
@@ -1,4 +1,7 @@
 # Drag
 
-This project was motivated by [this picture](https://static.boredpanda.com/blog/wp-content/uploads/2018/02/dwayne-the-rock-johnson-skyscraper-jump-funny-reactions-1-5a7ab25b4d416__700.jpg) of "The Rock" jumping between skyscrapers. We start with a rather rudimentary simulation of how objects fly through space and then try to make it progressively more realistic.
+This project was motivated by [this picture](https://static.boredpanda.com/blog/wp-content/uploads/2018/02/dwayne-the-rock-johnson-skyscraper-jump-funny-reactions-1-5a7ab25b4d416__700.jpg) of "The Rock" jumping between skyscrapers. We'd like to know if this jump is possible and what are limiting factors. Note that we will be reusing the `integrate.hpp` from the [n_body]() project, since we will need an ODE solver.
+
+### Current status of the project:
+Runs and produces reasonable results, but there seems to be a numerical instability: Depending on the initial conditions we get only roughly halt the possible solutions (all possible angles bigger or smaller than zero, but not the whole range). Some more investigation is necessary here. Also, need to add testing. 
 
--- drag/main.cpp
+++ drag/main.cpp
@@ -6,6 +6,118 @@
 #include <vector>
 #include <tuple>
 
+/*
+ * Function that perform the integration forward in time. 
+ * PRE:
+ *  - step:     specifies the method used to perform the integration
+ *  - rhs:      specifies the differential equation, i.e. the ODE has 
+ *              the form dz = rhs(t,z).
+ *  - z0:       initial conditions
+ *  - time_interval:
+ *              gives the timer-interval over which we integrate the ODE
+ *  - bins:     number of steps we take in the simulation. Higher number implies
+ *              higher precision, but also higher computation time. 
+ * POST:
+ *  - Each row of the resulting matrix contains the phase space coordinates at one
+ *    fixed time.  
+ */
+Eigen::MatrixXd integrate(step_t step, rhs_t rhs, phase_t const & z0,
+                          double const & time_interval, unsigned const & bins){
+    Eigen::MatrixXd z(bins+1, z0.size());
+    z.fill(0);
+    double const t0 = 0;
+    double const dt = time_interval/bins;
+    z.row(0) = z0;
+    for(std::size_t i = 0; i<bins; ++i) {
+        z.row(i + 1) = step(rhs, z.row(i), t0 + i * dt, dt);
+    }
+    phase_t t(bins+1);
+    for(std::size_t k = 0; k<t.size(); ++k){
+        t(k) = (k+1) * dt;
+    }
+    z.conservativeResize(Eigen::NoChange, z.cols()+1);
+    z.col(z.cols()-1) = t;
+    return z;
+}
+
+
+/*
+ For all methods we divide the computation into steps. Each step gets perform N times. 
+ The following function implement this step. Later, these will be called and summed up.
+ */
+
+/*
+  PRE:
+    - rhs:      f(t,z) for a given ODE.
+    - z0:       initial condition in phase space coordinates. 
+    - t0:       time at which we start the step
+    - dt:       time step, usually defined as time interval of the simulation
+                divided by number of total steps taken in the simulation.
+  POST:
+    - z+dz:     phase space type coordinates
+ */
+phase_t explicit_euler_step(rhs_t rhs, phase_t const & z0, double t0, double dt){
+    return z0 + dt * rhs(t0,z0);
+}
+
+phase_t explicit_midpoint_step(rhs_t rhs, phase_t const & z0, double t0, double dt){
+    phase_t tmp = z0;
+    tmp += (.5 * dt) * rhs(t0, z0);
+    return z0 + dt * rhs(t0 + .5*dt, tmp);
+}
+
+phase_t velocity_verlet_step(rhs_t rhs, phase_t const & z0, double t0, double dt) {
+    int const N = z0.size()/2/2;
+    phase_t x0 = z0.head(2*N);
+    phase_t v0 = z0.tail(2*N);
+    
+    phase_t x1 = x0 + dt*v0 + dt*dt/2. * rhs(t0,z0).tail(2*N);
+    phase_t tmp(4*N);
+    tmp << x1, v0;
+    phase_t v1 = v0 + dt/2. * (rhs(t0,z0)+rhs(t0+dt,tmp)).tail(2*N);
+    phase_t result(4*N);
+    result << x1,v1;
+    return result;
+}
+
+/*
+ Explicit Euler method.     For more details see: Wikipedia https://en.wikipedia.org/wiki/Euler_method
+ Explicit Midpoint method.  For more detials see: Wikipedia https://en.wikipedia.org/wiki/Midpoint_method
+ PRE:
+    - f:    characterizes the ODE. 
+    - z0:   initial conditions of the problem in form of phase-space
+            coordinates.
+    - T:    time interval you want to simulate, i.e. has to be a
+            positive real number.
+    - N:    number of individual steps you want to take in simulation.
+            The bigger N is the more precise the result will be and  
+            the longer the computation will take. 
+ POST:
+    - row k contains the phase space coordinates at time k*T/N.
+ */
+Eigen::MatrixXd explicit_euler(rhs_t rhs, phase_t const &z0, double const & T, unsigned const & N){
+    return integrate(explicit_euler_step,rhs,z0,T,N);
+}
+
+Eigen::MatrixXd explicit_midpoint(rhs_t rhs, phase_t const &z0, double const & T, unsigned const & N){
+    return integrate(explicit_midpoint_step,rhs,z0,T,N);
+}
+
+Eigen::MatrixXd velocity_verlet(rhs_t rhs, phase_t const &z0, double const & T, unsigned const & N){
+    return integrate(velocity_verlet_step,rhs,z0,T,N);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 // Small helper function to get Python's np.linspace
 template<typename T>
 std::vector<double> linspace(T start_in, T end_in, int num_in)
@@ -61,6 +173,7 @@ std::pair<double,double> calc_tstar_ref_and_x_ref_at_tstar_ref(double const & v0
 
 phase_t rhs_with_drag(double const & t, phase_t const & z){
     phase_t rhs(4);                                 // space for the rhs
+    rhs.fill(0);
     rhs.head(2) = z.tail(2);                        // fill velocities in first 2 elements of rhs
     phase_t tmp(2);
     double const v_norm = std::sqrt(z(2)*z(2)+z(3)*z(3));
@@ -81,7 +194,7 @@ std::tuple<double,double,double,int,std::pair<double,double>> try_tstar_drag(
     z0(1)=y_star;
     z0(2)=v0*std::cos(theta);
     z0(3)=v0*std::sin(theta);
-    Eigen::MatrixXd result = explicit_midpoint(rhs_with_drag, z0, tstar_search_grid.back(), tstar_search_grid.size());
+    Eigen::MatrixXd result = velocity_verlet(rhs_with_drag, z0, tstar_search_grid.back(), tstar_search_grid.size());
     phase_t y_drag = result.col(1);
     phase_t x_drag = result.col(0);
     int ierr=false;
@@ -124,10 +237,10 @@ std::tuple<double,double,double,bool> calc_x_star_with_drag_at_tstar(
                                            double const & y_star, 
                                            double t_star_est,
                                            double const eps_y=1.0e-3,
-                                           int const & number_of_t_searches=20,
+                                           int const & number_of_t_searches=2000,
                                            double const & rel_range_lower=0.8,
                                            double const & rel_range_upper=1.2,
-                                           int const & num_try=5){
+                                           int const & num_try=50){
     bool success = false;
     double t_star_est_lower=t_star_est*rel_range_lower;
     double t_star_est_upper=t_star_est*rel_range_upper;
@@ -174,7 +287,7 @@ std::tuple<double,double,bool,int> find_v0(double const & x_star,
                                            double const & y_star, 
                                            double t_star_est,
                                            double const & eps_x=1.0e-3,
-                                           double const & num_try=6){
+                                           double const & num_try=60){
     bool success = false;
     std::vector<double> v0_history;
     std::vector<double> x_star_history;
@@ -190,7 +303,7 @@ std::tuple<double,double,bool,int> find_v0(double const & x_star,
         
         v0_history.push_back(v0_est);
         x_star_history.push_back(x_drag);
-        if(success==false){                             //<-------------- probably wrong
+        if(success==true){                             //<-------------- probably wrong
             break;
         }
         if(std::abs(x_drag-x_star)<eps_x){
@@ -213,7 +326,7 @@ std::tuple<double,double,bool,int> find_v0(double const & x_star,
             }
         }
     }
-    return std::make_tuple(0,0,0,0);
+    return std::make_tuple(v0_est,t_star_est,success,number_of_tries);
 }
 
 int main(){
@@ -241,7 +354,7 @@ int main(){
      */
 
     // We fix the number of sample points
-    unsigned const theta_n = 51;
+    unsigned const theta_n = 50;
     double const theta_min = -0.1*M_PI;             // rad
     double const theta_max = 0.4*M_PI;              // rad
     unsigned const v0_n = 100;
@@ -287,6 +400,8 @@ int main(){
         }
     }
 
+    //for(auto e: v0_est) std::cout << e << "\n";
+
     // We compute now v0 for each theta under air drag
     std::vector<double> theta_sols;
     std::vector<double> t_star_sols;
@@ -294,6 +409,18 @@ int main(){
     for(std::size_t i=0; i<theta_n; i++){
         if(v0_est[i]>0.0){
             auto sol = find_v0(x_star, v0_est[i], theta_grid[i], y_star, t_star_est[i]);
+            bool success=std::get<2>(sol);
+            if(success==true){
+                theta_sols.push_back(theta_grid[i]);
+                t_star_sols.push_back(std::get<1>(sol));
+                v0_sols.push_back(std::get<0>(sol));
+            }
         }
+    }
+
+    for(std::size_t k=0; k<theta_sols.size(); k++){
+        std::cout << theta_sols[k]  << "\t" 
+                  << t_star_sols[k] << "\t"
+                  << v0_sols[k]     << "\n";
     }
 }
