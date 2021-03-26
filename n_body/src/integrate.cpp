514ad32e8e1d9b1d2f11012d1b6fb010ed7b239d
iilak <iilak@student.ethz.ch>
2021-03-26 14:50:03 +0100

    added templates for more general use

--- n_body/src/integrate.cpp
+++ n_body/src/integrate.cpp
@@ -15,64 +15,10 @@
  *  - Each row of the resulting matrix contains the phase space coordinates at one
  *    fixed time.  
  */
-Eigen::MatrixXd integrate(step_t step, rhs_t rhs, phase_t const & z0,
-                          double const & time_interval, unsigned const & bins){
-    Eigen::MatrixXd z(bins+1, z0.size());
-    z.fill(0);
-    double const t0 = 0;
-    double const dt = time_interval/bins;
-    z.row(0) = z0;
-    for(std::size_t i = 0; i<bins; ++i) {
-        z.row(i + 1) = step(rhs, z.row(i), t0 + i * dt, dt);
-    }
-    phase_t t(bins+1);
-    for(std::size_t k = 0; k<t.size(); ++k){
-        t(k) = (k+1) * dt;
-    }
-    z.conservativeResize(Eigen::NoChange, z.cols()+1);
-    z.col(z.cols()-1) = t;
-    return z;
-}
-
 
 /*
- For all methods we divide the computation into steps. Each step gets perform N times. 
- The following function implement this step. Later, these will be called and summed up.
- */
 
-/*
-  PRE:
-    - rhs:      f(t,z) for a given ODE.
-    - z0:       initial condition in phase space coordinates. 
-    - t0:       time at which we start the step
-    - dt:       time step, usually defined as time interval of the simulation
-                divided by number of total steps taken in the simulation.
-  POST:
-    - z+dz:     phase space type coordinates
- */
-phase_t explicit_euler_step(rhs_t rhs, phase_t const & z0, double t0, double dt){
-    return z0 + dt * rhs(t0,z0);
-}
-
-phase_t explicit_midpoint_step(rhs_t rhs, phase_t const & z0, double t0, double dt){
-    phase_t tmp = z0;
-    tmp += (.5 * dt) * rhs(t0, z0);
-    return z0 + dt * rhs(t0 + .5*dt, tmp);
-}
-
-phase_t velocity_verlet_step(rhs_t rhs, phase_t const & z0, double t0, double dt) {
-    int const N = z0.size()/2/3;
-    phase_t x0 = z0.head(3*N);
-    phase_t v0 = z0.tail(3*N);
-    
-    phase_t x1 = x0 + dt*v0 + dt*dt/2. * rhs(t0,z0).tail(3*N);
-    phase_t tmp(6*N);
-    tmp << x1, v0;
-    phase_t v1 = v0 + dt/2. * (rhs(t0,z0)+rhs(t0+dt,tmp)).tail(3*N);
-    phase_t result(6*N);
-    result << x1,v1;
-    return result;
-}
+*/
 
 /*
  Explicit Euler method.     For more details see: Wikipedia https://en.wikipedia.org/wiki/Euler_method
@@ -89,10 +35,12 @@ phase_t velocity_verlet_step(rhs_t rhs, phase_t const & z0, double t0, double dt
  POST:
     - row k contains the phase space coordinates at time k*T/N.
  */
+/*
 Eigen::MatrixXd explicit_euler(rhs_t rhs, phase_t const &z0, double const & T, unsigned const & N){
     return integrate(explicit_euler_step,rhs,z0,T,N);
 }
-
+*/
+/*
 Eigen::MatrixXd explicit_midpoint(rhs_t rhs, phase_t const &z0, double const & T, unsigned const & N){
     return integrate(explicit_midpoint_step,rhs,z0,T,N);
 }
@@ -100,3 +48,4 @@ Eigen::MatrixXd explicit_midpoint(rhs_t rhs, phase_t const &z0, double const & T
 Eigen::MatrixXd velocity_verlet(rhs_t rhs, phase_t const &z0, double const & T, unsigned const & N){
     return integrate(velocity_verlet_step,rhs,z0,T,N);
 }
+*/
